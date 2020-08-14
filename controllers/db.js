c157c96177036d5c42f39be4b437d10047a4bd40
Sampath Bandla <36627290+sampathbandla@users.noreply.github.com>
2020-08-14 13:41:11 +0530

    Added useUnifiedTopology: true

--- controllers/db.js
+++ controllers/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true,useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
